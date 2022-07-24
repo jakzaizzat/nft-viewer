9e44716ffeda97a0a23bf094a5229e6eb85463eb
jakzaizzat <jakzaizzat@gmail.com>
2022-07-24 23:11:29 +0800

    Added header

--- constant.ts
+++ constant.ts
@@ -2,6 +2,16 @@ export const BASE_API_URL = 'https://graphql.looksrare.org/graphql'
 
 // TODO: Replace with api key
 export const LOOKSRARE_HTTP_HEADER = {
+  accept: '*/*',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+  'content-type': 'application/json',
+  origin: 'https://looksrare.org',
+  referer: 'https://looksrare.org/',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  'sec-gpc': '1',
   'user-agent':
     'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
 }
